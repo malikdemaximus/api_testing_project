44612727edc89c831586fa404a060cf57f6fdb11
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-04-29 15:19:20 +0500

    Create api_client.py

---
+++ utils/api_client.py
@@ -0,0 +1,17 @@
+# utils/api_client.py
+
+import requests
+
+BASE_URL = "https://fakestoreapi.com"
+
+def get(endpoint):
+    return requests.get(f"{BASE_URL}/{endpoint}")
+
+def post(endpoint, data):
+    return requests.post(f"{BASE_URL}/{endpoint}", json=data)
+
+def put(endpoint, data):
+    return requests.put(f"{BASE_URL}/{endpoint}", json=data)
+
+def delete(endpoint):
+    return requests.delete(f"{BASE_URL}/{endpoint}")
