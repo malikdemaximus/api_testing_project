e86c4400390e904e65563998389ff90e14662ead
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-03 11:29:58 +0500

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,16 +1,19 @@
 # tests/test_users.py
 
 from utils.api_client import get
-import pytest
+from utils.schema_validator import validate_schema
 
 def test_get_all_users_status_code():
     response = get("users")
     assert response.status_code == 200
 
 def test_user_fields_exist():
-    response = get("users")
-    user = response.json()[0]
+    user = get("users").json()[0]
     assert "id" in user
     assert "email" in user
     assert "username" in user
     assert "address" in user
+
+def test_validate_user_schema():
+    user = get("users/1").json()
+    validate_schema(user, "data/schemas/user_schema.json")
